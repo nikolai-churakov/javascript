6edbc980b19b7b2987008de97ebb90bf33775d88
nikolai-churakov <cni.zip@gmail.com>
2022-02-03 18:00:36 +0300

    добавлена рекурсивная функция

---
+++ recursiveFunction.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// Дано натуральное число n. Напишите рекурсивную функцию, которая возвращает строку чисел от 1 до n, разделенных space.
+//
+
+function my_recursive (n) {
+    if (n>0) {
+        my_recursive(n-1);
+        document.write(n + ' ');
+    }
+}
