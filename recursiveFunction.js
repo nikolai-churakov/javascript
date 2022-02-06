c1fd4b42cb154a031484e2ef790d1c72ac3bcff7
nikolai-churakov <cni.zip@gmail.com>
2022-02-06 20:22:22 +0300

    finish task 2 from lesson 2

--- recursiveFunction.js
+++ recursiveFunction.js
@@ -1,27 +1,27 @@
 "use strict";
-
 // Дано натуральное число n. Напишите рекурсивную функцию, которая возвращает строку чисел от 1 до n, разделенных space.
 // test script
-let n = 12;
-function my_recursive (n) {
-    if (n>0) {
-        my_recursive(n-1);
-        document.write(n + ' ');
-    }
-}
-function my_function(n) { return n <= 1 ? 1 : my_function(n - 1) + ' ' + n; }
-console.log(my_function(9));
-
-
-let age = 29;
-let status = (age >= 18) ? "adult" : "minor";
-console.log(status);
-
-function fibonacci(n, prev1, prev2){
-    var current = prev1 + prev2;
-    var fibonacci_string = current + " ";
-    if(n > 1)
-        fibonacci_string += fibonacci(n - 1, current, prev1);
-    return fibonacci_string;
-}
-alert(fibonacci(15, 1, 0));
+// let n = 12;
+// function my_recursive (n) {
+//     if (n>0) {
+//         my_recursive(n-1);
+//         document.write(n + ' ');
+//     }
+// }
+// function my_function(n) { return n <= 1 ? 1 : my_function(n - 1) + ' ' + n; }
+// console.log(my_function(9));
+//
+//
+// let age = 29;
+// let status = (age >= 18) ? "adult" : "minor";
+// console.log(status);
+//
+// function fibonacci(n, prev1, prev2){
+//     var current = prev1 + prev2;
+//     var fibonacci_string = current + " ";
+//     if(n > 1)
+//         fibonacci_string += fibonacci(n - 1, current, prev1);
+//     return fibonacci_string;
+// }
+// alert(fibonacci(15, 1, 0));
+ленные произвольные начальные значен
