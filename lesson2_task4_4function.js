c70c0b1c561cd295573e7eab07c5866ad4e62f86
nikolai-churakov <cni.zip@gmail.com>
2022-02-08 21:35:03 +0300

    add task 4 (4 function + return)

---
+++ lesson2_task4_4function.js
@@ -0,0 +1,42 @@
+"use strict";
+
+// Реализовать основные 4 арифметические операции (+, -, /, *) в виде функций с двумя параметрами.
+// Обязательно использовать оператор return.
+
+const firstNumber = 6;
+const secondNumber = 2;
+
+//  Функция суммы.
+function add ()
+{
+    console.log('Сумма значений = ' + `${firstNumber + secondNumber}`);
+    return (firstNumber + secondNumber);
+}
+add();
+
+
+//  * Функция вычитания.
+function reduce ()
+{
+    console.log('Сумма значений = ' + `${firstNumber - secondNumber}`);
+    return (firstNumber - secondNumber);
+}
+reduce();
+
+
+//  * Функция деления.
+function divide ()
+{
+    console.log('Сумма значений = ' + `${firstNumber / secondNumber}`);
+    return (firstNumber / secondNumber);
+}
+divide();
+
+
+//  * Функция умножения.
+function multiply ()
+{
+    console.log('Сумма значений = ' + `${firstNumber * secondNumber}`);
+    return (firstNumber * secondNumber);
+}
+divide();
