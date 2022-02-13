1d5840afb72f3205b16f14e4cad0fa30ce319735
nikolai-churakov <cni.zip@gmail.com>
2022-02-13 19:10:14 +0300

    add fix task6

--- lesson2_task6_getMoneyToDeposit.js
+++ lesson2_task6_getMoneyToDeposit.js
@@ -1,12 +1,16 @@
 "use strict";
 
-const replenishmentAmount = prompt('Выбирите сумму для пополнения ( 101 руб., 10020 руб. или 120104 руб.', '');
+const successfullyEnrolled = 'успешно зачислена.';
+const selectionOptionSum = 'Выбраная из вариантов сумма в';
+const selectionSum = 'Выбраная из вариантов сумма в';
+
+const replenishmentAmount = prompt('Выбирите сумму пополнения ( 101 руб., 10020 руб. или 120104 руб.', '');
 // проверка на валидность replenishmentAmount
 
 /**
- *
+ * Определяет падеж валюты
  * @param amountMoney
- * @return
+ * @returns Правильная падежная форма рубля
  */
 const getCurrency = (amountMoney) => {
     const lastSymbol = amountMoney[amountMoney.length - 1];
@@ -20,31 +24,23 @@ const getCurrency = (amountMoney) => {
     }
 }
 
-
+/**
+ * Выводит зачисленную сумму
+ * @param amountMoney
+ */
 const getMoneyToDeposit = (amountMoney) => { // amountMoney = '100500'
     const currency = getCurrency(amountMoney);
     const isCustomMoney = amountMoney === 101 || amountMoney === 10020 || amountMoney === 120104;
 
     switch (true) {
         case isCustomMoney:
-            console.log(`Выбраная из вариантов сумма в ${amountMoney} ${currency} успешно зачислена.`);
+            console.log(`${selectionOptionSum} ${amountMoney} ${currency}. ${successfullyEnrolled}`);
             break;
         default:
-            console.log(`Ваша сумма в ${amountMoney} ${currency}. Успешно зачислена.`);
+            console.log(`${selectionSum} ${amountMoney} ${currency}. ${successfullyEnrolled}`);
     }
 }
 
 if (!Number.isNaN(+replenishmentAmount)) {
     getMoneyToDeposit(replenishmentAmount);
 }
-
-
-// Урок 2. Задание 6.
-// Программа должна спросить у пользователя количество денег, которое он хочет положить в банк.
-// Пользователь должен ввести целое число, а программа должна выдать соответствующее сообщение:
-// "Ваша сумма в 101 рубль успешно зачислена." - в случае если пользователь ввел 101.
-// "Ваша сумма в 10020 рублей успешно зачислена." - в случае если пользователь ввел 10020.
-// "Ваша сумма в 120104 рубля успешно зачислена." - в случае если пользователь ввел 120104.
-// Если пользователь введет любое другое целое число, программа также должна выдать соответствующее сообщение,
-// в котором будет отображено это число, а также поставить верное окончание в слове "рубль".
-// Для получения верного склонения слова (любого слова с числом) вам необходимо создать функцию.
