dcf71637d294be0f6b988d16c4bdd2c678029191
nikolai-churakov <cni.zip@gmail.com>
2022-02-14 21:55:46 +0300

    +2 f

--- tempLearn.js
+++ tempLearn.js
@@ -29,3 +29,24 @@ if (n < 1) {
 } else {
     alert( pow(x, n) );
 }
+
+/**
+ *
+ * @param n
+ */
+function showPrimes(n) {
+
+    for (let i = 2; i < n; i++) {
+        if (!isPrime(i)) continue;
+
+        alert(i);  // простое
+    }
+}
+
+function isPrime(n) {
+    for (let i = 2; i < n; i++) {
+        if ( n % i == 0) return false;
+    }
+    return true;
+}
+showPrimes(5);
