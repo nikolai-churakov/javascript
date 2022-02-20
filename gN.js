e009644c1a2bb9bdb8b24e856b6e1255550177f0
nikolai-churakov <cni.zip@gmail.com>
2022-02-20 20:05:23 +0300

    lesson3_task2.js

---
+++ gN.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let generatedNumbers;
+let attemptsCount;
+
+resetGame();
+
+function resetGame() {
+    attemptsCount = 0;
+    generatedNumbers = [];
+
+    while (generatedNumbers.length < 4) {
+
+    }
+}
--- lesson3_task2.js
+++ lesson3_task2.js
@@ -4,15 +4,15 @@
 // for(…){/* здесь пусто */}
 // ```
 "use strict";
-let arr = [
-    ['чураков', 'николай', 7177872, 34],
-    [true, 'москва', 'ЮЗАО', 261],
-    [13, 0o3, 101, 777000],
-];
-
-for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr[i].length; j++) {
-        console.log(arr[i][j]);
-    }
-}
-console.log(arr.length);
+// let arr = [
+//     ['чураков', 'николай', 7177872, 34],
+//     [true, 'москва', 'ЮЗАО', 261],
+//     [13, 0o3, 101, 777000],
+// ];
+//
+// for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr[i].length; j++) {
+//         console.log(arr[i][j]);
+//     }
+// }
+// console.log(arr.length);
