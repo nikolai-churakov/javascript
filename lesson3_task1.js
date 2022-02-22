0582efc0ce9c1679ebce9d24b26eb52b096cc31e
nikolai-churakov <cni.zip@gmail.com>
2022-02-22 08:25:21 +0300

    task 1. do while. finish

--- lesson3_task1.js
+++ lesson3_task1.js
@@ -8,3 +8,24 @@
 // …
 // 10 – четное число
 // ```
+
+/**
+ *  Цикл выводит ряд значений от 0 до 10
+ * @type {string}
+ */
+let result = '';
+let i = 0;
+
+do {
+    if (i === 0) {
+        console.log(`${i} - это ноль`);
+    }
+    if (i % 2) {
+        console.log(`${i} - это нечетное`);
+    } else if ( i !==0) {
+        console.log(`${i} - это четное`);
+    }
+    result = result + i;
+    i++;
+} while (i < 11);
+
