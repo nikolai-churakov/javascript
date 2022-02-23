609e76d3c4f327c8721d55bad736282ff926c95d
nikolai-churakov <cni.zip@gmail.com>
2022-02-23 20:42:42 +0300

    3

--- lesson3_task3.js
+++ lesson3_task3.js
@@ -4,5 +4,9 @@
 /**
  * Выводит список значений от 0 до 9. Вся логика задана в условии цикла.
  */
-for ( let i = 0; i < 10; console.log(i), i++) {
+for ( let i = 0; i < 20; i++ ) {
+    let slide = X;
+    const xx = "XX";
+    console.log(slide);
+    slide = slide + xx;
 }
