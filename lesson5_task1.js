d50addc57d6ce6c55a8f6a4e852bbda5fd53f3bd
nikolai-churakov <cni.zip@gmail.com>
2022-03-16 22:28:13 +0300

    выполнил корректировку по замечаниям шефа

--- lesson5_task1.js
+++ lesson5_task1.js
@@ -1,10 +1,24 @@
 'use strict';
 
 const settings = {
-
+    rowCount: 10,
+    colsCount: 10,
+    startPositionX: 0,
+    startPositionY: 0,
+    startDirection: 'right',
+    stepInSecond: 5,
+    playerCellColor: 'AA3333',
+    emtyCellColor: 'EEEEEE',
 };
 
 const = player {
+    x: null,
+    y: null,
 
 };
-Event.target
+
+const game = {
+
+};
+
+game.run();
