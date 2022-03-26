57bc6da00d8f6db1f0dd00cd9ee63cd4ff50ff37
nikolai-churakov <cni.zip@gmail.com>
2022-03-26 16:12:59 +0300

    add 3

--- inspectForm.js
+++ inspectForm.js
@@ -1,11 +1,25 @@
 'use strict';
 
 class CheckForm {
-    constructor(name, phone, email, message) {
+    constructor(name, phone, password) {
         this.name = name;
         this.phone = phone;
-        this.email = email;
-        this.message = message;
-        this.counter = 0;
+        this.password = password;
+    }
+
+    init() {
+        this.NameCheck();
+        this.PhoneCheck();
+        this.PasswordCheck();
+        this.AlarmMessage();
+    }
+
+    NameCheck() {
+        let nameValue = document.getElementById(this.name).value;
+        if (/^[a-z]{2,}$/ig.test(nameValue)) {
+            this.isRight(this.name);
+        } else {
+            this.isWrong(this.name);
+        }
     }
 }
