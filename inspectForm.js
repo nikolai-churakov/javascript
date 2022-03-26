732cdff3572446a7fb0fba5798b59c61e341087c
nikolai-churakov <cni.zip@gmail.com>
2022-03-26 16:15:30 +0300

    add 3

--- inspectForm.js
+++ inspectForm.js
@@ -10,7 +10,7 @@ class CheckForm {
     init() {
         this.NameCheck();
         this.PhoneCheck();
-        this.PasswordCheck();
+        this.passwordCheck();
         this.AlarmMessage();
     }
 
@@ -22,4 +22,41 @@ class CheckForm {
             this.isWrong(this.name);
         }
     }
+
+    PhoneCheck() {
+        let phoneValue = document.getElementById(this.phone).value;
+        if (/^\+\d\(\d{3}\)\d{3}\-\d{4}/ig.test(phoneValue)){
+            this.isRight(this.phone);
+        } else this.isWrong(this.phone);
+    }
+
+
+    passwordCheck() {
+        let emailValue = document.getElementById(this.email).value;
+        if (/^[a-z]+(\.|\_|)mail@[a-z]+\.(com|ru)/ig.test(emailValue)){
+            this.isRight(this.email);
+        } else this.isWrong(this.email);
+    }
+
+    isRight(id){
+        document.getElementById(id).style.borderColor = 'green';
+    }
+
+    isWrong(id){
+        document.getElementById(id).style.borderColor = 'red';
+        this.counter = this.counter + 1;
+    }
+
+    AlarmMessage(){
+        if (this.counter > 0) {
+            document.getElementById('alarmMessage').textContent = 'Введите данные в верном формате';
+        }
+    }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('checkBtn').addEventListener('click', () => {
+        let checkForm = new CheckForm ('name', 'phone', 'password', 'message');
+        checkForm.init();
+    })
+})
