f121dfb3ff0334fe9cce38d1a35760bc53a407f2
nikolai-churakov <cni.zip@gmail.com>
2022-03-27 11:27:05 +0300

    add 3

--- inspectForm.js
+++ inspectForm.js
@@ -1,7 +1,5 @@
-'use strict';
-
 class CheckForm {
-    constructor(name, phone, password) {
+    constructor (name, phone, password) {
         this.name = name;
         this.phone = phone;
         this.password = password;
@@ -9,50 +7,50 @@ class CheckForm {
     }
 
     init() {
-        this.NameCheck();
-        this.PhoneCheck();
+        this.nameCheck();
+        this.phoneCheck();
         this.passwordCheck();
         this.AlarmMessage();
     }
 
-    NameCheck() {
+    nameCheck() {
         let nameValue = document.getElementById(this.name).value;
         if (/^[a-z]{2,}$/ig.test(nameValue)) {
             this.isRight(this.name);
         } else {
             this.isWrong(this.name);
         }
-    }
+    };
 
-    PhoneCheck() {
+    phoneCheck() {
         let phoneValue = document.getElementById(this.phone).value;
-        if (/^\+\d\(\d{3}\)\d{3}\-\d{4}/ig.test(phoneValue)){
-            this.isRight(this.phone);
-        } else this.isWrong(this.phone);
-    }
+        if (/^\+?[78][-\(]?\d{3}\)?-?\d{3}-?\d{2}-?\d{2}$/ig.test(phoneValue)){
+            this.isRight(phone);
+        } else this.isWrong(phone);
+    };
 
 
     passwordCheck() {
-        let emailValue = document.getElementById(this.email).value;
-        if (/^[a-z]+(\.|\_|)mail@[a-z]+\.(com|ru)/ig.test(emailValue)){
-            this.isRight(this.email);
-        } else this.isWrong(this.email);
-    }
+        let emailValue = document.getElementById(password).value;
+        if (/^[a-z]+(\.|\_|)mail@[a-z]+\.(com && ru)/ig.test(passwordValue)){
+            this.isRight(password);
+        } else this.isWrong(password);
+    };
 
     isRight(id){
         document.getElementById(id).style.borderColor = 'green';
-    }
+    };
 
     isWrong(id){
         document.getElementById(id).style.borderColor = 'red';
-        this.counter = this.counter + 1;
-    }
+        this.counter = counter + 1;
+    };
 
     AlarmMessage(){
-        if (this.counter > 0) {
+        if (counter > 0) {
             document.getElementById('alarmMessage').textContent = 'Не верный формат данных';
         }
-    }
+    };
 }
 
 document.addEventListener('DOMContentLoaded', () => {
