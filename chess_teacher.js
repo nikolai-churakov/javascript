523dc169306dd75ff86987d40b3b22785d1c35cf
nikolai-churakov <cni.zip@gmail.com>
2022-03-30 21:20:00 +0300

    add board in table html

--- chess_teacher.js
+++ chess_teacher.js
@@ -7,8 +7,20 @@ const chess = {
     gameContainerEl: document.getElementById('board'),
 
     renderMap() {
-        console.log(123);
+        const rows = [0, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+        const cols = [0, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 0];
+
+        for (let row = 0; row < rows.length; row++) {
+            const tr = document.createElement('tr');
+            this.gameContainerEl.appendChild(tr);
+
+            for (let col = 0; col < cols.length; col++) {
+                const td = document.createElement('tr');
+                tr.appendChild(td);
+            }
+        }
     }
-};
+}
 
 chess.renderMap();
+
