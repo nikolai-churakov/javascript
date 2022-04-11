e0004a770d17b36fd983d881be7f03983ff88e08
nikolai-churakov <cni.zip@gmail.com>
2022-04-11 17:46:46 +0300

    finish match block for cart

--- cart/script.js
+++ cart/script.js
@@ -28,7 +28,6 @@ document.onclick = event => {
     if (event.target.classList.contains('plus-btn')) {
         console.log("нажал добавить",event.target.dataset.id);
         plusFunction(event.target.dataset.id);
-        // document.querySelector(this.dataset.id).innerHTML = 5;
     }
     if (event.target.classList.contains('minus-btn')) {
         console.log(event.target.dataset.id);
@@ -51,8 +50,6 @@ const minusFunction = id => {
 }
 
 const deleteFunction = id => {
-    // delete document.getElementById('test1');
-    // delete document.querySelector( '.item[data-content="'+id+'"]');
     console.log(document.querySelector( '.item[data-content="'+id+'"]'));
     delete cart[id];
     renderCart(id);
@@ -60,27 +57,18 @@ const deleteFunction = id => {
 
 
 const renderCart = (id) => {
-    // console.log(cart[id]['count']);
-    // console.log(id);
-    // document.querySelector('basket-count').innerText = this.count
     let new_count = document.querySelector("span[data-id='" + id + "']").innerText = cart[id]['count'];
     totalPrice(id, new_count);
-    // asd = cart[id]['count'];
-    // console.log(asd);
+
 }
-    // let new_count = document.querySelector(".count");
-    // "div.user-panel.main input[name=login]"
-    // < spanclassName = "count"data - id = "ART020304231" > 1 < /span>
-    //new_count.innerHTML = cart[id]['count'];
+
 
 const totalPrice = (id, new_count) => {
     let cart_price = cart[id]['price'];
     let totalPrice = cart_price * new_count;
     console.log(totalPrice);
-    let ttt= document.querySelectorAll(".total-price");
+    let ttt = document.querySelector(".total-price[data-id='" + id + "']").innerText = totalPrice;
     console.log(ttt);
-    //.innerText = totalPrice;
-    // document.querySelector(".total-price[data-id='" + id + "']").innerText = (new_total * cart[id]['price']);
 }
 
 
