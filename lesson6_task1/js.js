f437f9b5adb60724646866b3291add95fa01a4ff
nikolai-churakov <cni.zip@gmail.com>
2022-04-28 21:36:44 +0300

    remove event

--- lesson6_task1/js.js
+++ lesson6_task1/js.js
@@ -24,8 +24,8 @@ const gallery = {
         document
             .querySelector(this.settings.previewSelector)
             .addEventListener('click', event => this.containerClickHandler(event));
-            // .addEventListener('click', event =>  direction);
             // const direction = this.getDirectionByCode(event.code);
+
 
         this.arrayConstraction();
     },
