6cfc81332727449be2c073e5207aecc1ab14fe2d
nikolai-churakov <cni.zip@gmail.com>
2022-05-02 17:26:50 +0300

    add oop ann

--- jsProLesson1/lesson1.js
+++ jsProLesson1/lesson1.js
@@ -10,10 +10,11 @@
 function Person (name, yearOfBirth) {
     this.name =  name;
     this.yearOfBirth = yearOfBirth;
-    this.calcAge = function () {
-        return console.log(new Date().getFullYear() - this.yearOfBirth)
-    }
 }
+
+Person.prototype.calcAge =  function () {
+    return console.log(new Date().getFullYear() - this.yearOfBirth)
+};
 
 let Nick = new  Person('Nick', 1987);
 let Ann = new Person ('Ann', 1991);
