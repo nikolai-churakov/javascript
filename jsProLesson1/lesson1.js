93af736ea2b00a885b2c1097d53a2f14e90efa0b
nikolai-churakov <cni.zip@gmail.com>
2022-05-02 10:41:38 +0300

    add obj OOP

--- jsProLesson1/lesson1.js
+++ jsProLesson1/lesson1.js
@@ -1,6 +1,18 @@
 "use strict"
 
-let person = {
-    name: name,
-    yearOfBirth: yearOfBirth,
-};
+//ES5
+
+function Person (name, yearOfBirth) {
+    this.name =  name;
+    this.calcAge = function () {
+        return console.log(new Date() - this.yearOfBirth)
+    }
+}
+
+
+
+//
+// let person = {
+//     name: name,
+//     yearOfBirth: yearOfBirth,
+// };
