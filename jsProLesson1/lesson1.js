8ba3e1adfcd4b72087e8fdb4ee6141920b0323a5
nikolai-churakov <cni.zip@gmail.com>
2022-05-02 17:45:42 +0300

    add oop ann

--- jsProLesson1/lesson1.js
+++ jsProLesson1/lesson1.js
@@ -1,12 +1,6 @@
 "use strict"
 
 //ES5
-/**
- * Объект ООП
- * @param name
- * @param yearOfBirth
- * @constructor
- */
 function Person (name, yearOfBirth) {
     this.name =  name;
     this.yearOfBirth = yearOfBirth;
@@ -16,5 +10,8 @@ Person.prototype.calcAge =  function () {
     return console.log(new Date().getFullYear() - this.yearOfBirth)
 };
 
+Person.prototype.lastName = 'Smith';
+
 let Nick = new  Person('Nick', 1987);
 let Ann = new Person ('Ann', 1991);
+
