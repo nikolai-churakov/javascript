85cd383045384f92a0074e45875a37d3f44e2d8a
nikolai-churakov <cni.zip@gmail.com>
2022-05-02 18:01:24 +0300

    add ES6

--- jsProLesson1/lesson1.js
+++ jsProLesson1/lesson1.js
@@ -1,25 +1,28 @@
 "use strict"
 
 //ES5
-function Person (name, yearOfBirth) {
-    this.name =  name;
-    this.yearOfBirth = yearOfBirth;
-}
+// function Person (name, yearOfBirth) {
+//     this.name =  name;
+//     this.yearOfBirth = yearOfBirth;
+// }
+//
+// Person.prototype.calcAge =  function () {
+//     console.log(new Date().getFullYear() - this.yearOfBirth)
+// };
+//
+// Person.prototype.lastName = 'Smith';
+//
+// function Teacher(name, yearOfBirth, subject) {
+//     Person.call(this, name, yearOfBirth);
+//     this.subject = subject;
+// }
+//
+// Teacher.prototype = Object.create(Person.prototype);
+// Teacher.prototype.constructor = Teacher;
+//
+// let Nick = new  Person('Nick', 1987);
+// let Ann = new Teacher('Ann', 1991, 'math');
 
-Person.prototype.calcAge =  function () {
-    console.log(new Date().getFullYear() - this.yearOfBirth)
-};
+//ES6
 
-Person.prototype.lastName = 'Smith';
-
-function Teacher(name, yearOfBirth, subject) {
-    Person.call(this, name, yearOfBirth);
-    this.subject = subject;
-}
-
-Teacher.prototype = Object.create(Person.prototype);
-Teacher.prototype.constructor = Teacher;
-
-let Nick = new  Person('Nick', 1987);
-let Ann = new Teacher('Ann', 1991, 'math');
 
