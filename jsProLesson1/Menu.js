607cb803beb5291617ef58e1d260272f52e3139e
nikolai-churakov <cni.zip@gmail.com>
2022-05-03 06:25:57 +0300

    add let result

---
+++ jsProLesson1/Menu.js
@@ -0,0 +1,10 @@
+class Menu {
+    constructor(id,className, items) {
+        this.id = id;
+        this.className = ClassName;
+        this.items - items;
+    }
+    render(){
+        let result = `<ul class="${this.className}" id="${this.id}"`
+    }
+}
