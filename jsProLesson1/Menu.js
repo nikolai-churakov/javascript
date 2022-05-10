4fcea67025140b118f68194eb440ca494224549a
nikolai-churakov <cni.zip@gmail.com>
2022-05-10 11:53:17 +0300

    lesson1 oop

--- jsProLesson1/Menu.js
+++ jsProLesson1/Menu.js
@@ -1,8 +1,9 @@
 class Menu {
-    constructor(id, className, items) {
+    constructor(id, className, items, subItems) {
         this.id = id;
         this.className = className;
         this.items = items;
+        this.subItems = subItems;
     }
 
     render(){
@@ -10,10 +11,10 @@ class Menu {
         for (let i=0; i < this.items.length; i++) {
             if(this.items[i] instanceof MenuItem) {
                 result += this.items[i].render();
-                for (let j=0; j < this.subItems.length; j++) {
-                    result += `${this.items[i][j].render()}`;
-                }
-                result += `</ul>`;
+            //     for (let j=0; j < this.subItems.length; j++) {
+            //         result += `${this.items[i][j].render()}`;
+            //     }
+            //     result += `</ul>`;
             }
         }
         result += `</ul>`;
