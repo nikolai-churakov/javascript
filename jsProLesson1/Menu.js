bad34990571005ddb20355c1d27d7ab1d196028a
nikolai-churakov <cni.zip@gmail.com>
2022-05-19 21:26:09 +0300

    add HW menu

--- jsProLesson1/Menu.js
+++ jsProLesson1/Menu.js
@@ -1,34 +1,54 @@
 class Menu {
-    constructor(id, className, items, subItems) {
+    constructor(id, className, items) {
         this.id = id;
         this.className = className;
         this.items = items;
-        this.subItems = subItems;
     }
-
     render() {
         let result = `<ul class="${this.className}" id="${this.id}">`;
         for (let i=0; i < this.items.length; i++) {
-            if(this.items[i] instanceof MenuItem) {
-                result += this.items[i].render();
-
-            } else if(this.items[i] instanceof MenuSubItem) {
-                result += this.items[i].render();
+            if(this.items[i] instanceof MenuItem || this.items[i] instanceof SubMenu) {
+                result += this.items[i].render()
             }
         }
         result += `</ul>`;
-        console.log(this.items);
-        console.log(this.subItems);
-        return result;
+        return result
+    }
+    remove() {
+        let el = document.getElementById(this.id);
+        if (el) {
+            el.remove();
+        }
+    }
+}
+class SubMenu extends Menu {
+    constructor(href, title, id, className, items) {
+        super(id, className, items);
+        this.href = href;
+        this.title = title;
+    }
+    render(){
+        return `<li><a href="${this.href}">${this.title}</a>${super.render()}</li>`
     }
 }
 
 document.addEventListener('keydown', function(event) {
-    if (event.code === 'Delete' || event.code === 'Backspace') {
-        document.getElementById('menuBlock').remove();
-    } else if (event.code === 'KeyQ'){
-        document.getElementById('submenu-block').remove();
-    } else {
-        console.log('На эту клавишу события не предусмотренно.');
+    let menuEl = document.getElementById('menuBlock');
+    if (menuEl) {
+        switch (event.code) {
+            case 'Delete':
+            document.getElementById('menuBlock').remove();
+            break;
+            case 'Backspace':
+            document.getElementById('menuBlock').remove();
+            break;
+        }
     }
+    // if (event.code === 'Delete' || event.code === 'Backspace') {
+    //     document.getElementById('menuBlock').remove();
+    // } else if (event.code === 'KeyQ'){
+    //     document.getElementById('submenu-block').remove();
+    // } else {
+    //     console.log('На эту клавишу события не предусмотренно.');
+    // }
 });
