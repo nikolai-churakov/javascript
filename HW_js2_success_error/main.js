8854dfe5150bc7e21710327409e6974ba4329b7b
nikolai-churakov <cni.zip@gmail.com>
2022-05-22 14:46:19 +0300

    add main js class request

---
+++ HW_js2_success_error/main.js
@@ -0,0 +1,37 @@
+class Request {
+
+    init() {
+        this.successRequest();
+        this.errorRequest()
+    }
+
+    successRequest() {
+        document.getElementById('success').addEventListener('click', () => {
+            let xhr = new XMLHttpRequest();
+            xhr.open('GET', '../success.json', true);
+            xhr.onload = () => {
+                let data = JSON.parse(xhr.responseText);
+                console.log(data);
+            }
+            xhr.send();
+        })
+    }
+
+    errorRequest() {
+        document.getElementById('error').addEventListener('click', () => {
+            let xhr = new XMLHttpRequest();
+            xhr.open('GET', '../error.json', true);
+            xhr.onload = () => {
+                let data = JSON.parse(xhr.responseText);
+                console.log(data);
+            }
+            xhr.send();
+        })
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    let req = new Request();
+    req.init();
+})
+
