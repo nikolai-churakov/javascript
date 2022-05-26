24f602e37509bffdbdfaacad60361a49b41f95c9
nikolai-churakov <cni.zip@gmail.com>
2022-05-26 20:57:19 +0300

    add fetch

--- HW_js2_success_error/main.js
+++ HW_js2_success_error/main.js
@@ -6,42 +6,42 @@ class Request {
         this.AboutMe();
     }
 
-    successRequest() {
-        document.getElementById('success').addEventListener('click', () => {
-            let xhr = new XMLHttpRequest();
-            xhr.open('GET', 'success.json', true);
-            xhr.onload = () => {
-                let data = JSON.parse(xhr.responseText);
-                console.log(data);
-            }
-            xhr.send();
-        })
-    }
-
-    errorRequest() {
-        document.getElementById('error').addEventListener('click', () => {
-            let xhr = new XMLHttpRequest();
-            xhr.open('GET', 'error.json', true);
-            xhr.onload = () => {
-                let data = JSON.parse(xhr.responseText);
-                console.log(data);
-            }
-            xhr.send();
-        })
-    }
-
-    AboutMe() {
-        document.getElementById('aboutMe').addEventListener('click', () =>  {
-            let getInfo = new XMLHttpRequest();
-            getInfo.open('GET', 'help.json', true);
-            getInfo.onload = () => {
-                let data = JSON.parse(getInfo.responseText);
-                console.log(data);
-            }
-            getInfo.send();
-        })
-    }
-}
+//     successRequest() {
+//         document.getElementById('success').addEventListener('click', () => {
+//             let xhr = new XMLHttpRequest();
+//             xhr.open('GET', 'success.json', true);
+//             xhr.onload = () => {
+//                 let data = JSON.parse(xhr.responseText);
+//                 console.log(data);
+//             }
+//             xhr.send();
+//         })
+//     }
+//
+//     errorRequest() {
+//         document.getElementById('error').addEventListener('click', () => {
+//             let xhr = new XMLHttpRequest();
+//             xhr.open('GET', 'error.json', true);
+//             xhr.onload = () => {
+//                 let data = JSON.parse(xhr.responseText);
+//                 console.log(data);
+//             }
+//             xhr.send();
+//         })
+//     }
+//
+//     AboutMe() {
+//         document.getElementById('aboutMe').addEventListener('click', () =>  {
+//             let getInfo = new XMLHttpRequest();
+//             getInfo.open('GET', 'help.json', true);
+//             getInfo.onload = () => {
+//                 let data = JSON.parse(getInfo.responseText);
+//                 console.log(data);
+//             }
+//             getInfo.send();
+//         })
+//     }
+// }
 
 document.addEventListener('DOMContentLoaded', () => {
     let req = new Request();
