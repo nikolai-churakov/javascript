af000352bd98da25a0b967ccfc350a56d56ca98f
nikolai-churakov <cni.zip@gmail.com>
2022-05-27 19:17:29 +0300

    add fetch

--- HW_js2_success_error/main.js
+++ HW_js2_success_error/main.js
@@ -1,31 +1,43 @@
+window.onload = () => {
+    let buttonSendForm = document.getElementById('success');
+    buttonSendForm.addEventListener('click', e => {
+        e.preventDefault();
+        let request = 'error.json';
+        if (document.getElementById('input').value){
+            request = 'success.json';
+        }
+        fetch(request)
+            .then(result => result.json())
+            .then(data => console.log(data.result))
 
+    })
+}
 
-
-document.getElementById('success').addEventListener('click', () => {
+// document.getElementById('success').addEventListener('click', () => {
 
     // let gerData = new Promise((resolve, reject) => {
     //
     //
     // }
 
-let dataBlock = document.getElementById('infoBox');
-
-    fetch('succes1s.json')
-        .then(result => {
-            console.log('all right');
-            console.log(result);
-            return result.json()
-        })
-        .then(data => {
-            console.log(data);
-            dataBlock.innerText = `Телефон (${data.tel})`;
-
-        })
-        .catch(error => {
-            console.log(error);
-            console.log(data);
-        })
-})
+// let dataBlock = document.getElementById('infoBox');
+//
+//     fetch('succes1s.json')
+//         .then(result => {
+//             console.log('all right');
+//             console.log(result);
+//             return result.json()
+//         })
+//         .then(data => {
+//             console.log(data);
+//             dataBlock.innerText = `Телефон (${data.tel})`;
+//
+//         })
+//         .catch(error => {
+//             console.log(error);
+//             console.log(data);
+//         })
+// })
 
 // class Request {
 // init() {
